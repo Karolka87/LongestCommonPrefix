c9fdae218cabcb252fe1e28fc7049c051d2df1cd
Karolka87 <115976221+Karolka87@users.noreply.github.com>
2023-02-27 11:10:33 +0100

    leetcode14

---
+++ src/LongestCommonPrefix.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class LongestCommonPrefix {
+    public static void main(String[] args){
+        String[] lista1 = new String[5];
+        lista1[0] = "cypr";
+        lista1[1] = "cygaro";
+        lista1[2] = "cyniczny";
+        lista1[3] = "pomidor";
+        lista1[4] = "gruszka";
+
+        Arrays.sort(lista1);
+        System.out.println(Arrays.toString(lista1));
+        String t1 = lista1[0];
+        String t2 = lista1[lista1.length - 1];
+        int idx = 0;
+        while (idx < t1.length() && idx < t2.length()) {
+            if(t1.charAt(idx) == t2.charAt(idx)) {
+                idx++;
+            }
+            else {
+                break;
+            }
+        }
+        System.out.println(t1.substring(0, idx));
+
+    }
+}
